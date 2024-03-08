2d3e9a3ce1ebb1fa6d20b5f929f7213d797e86f4
Omi Johnson <ojohnson@bigelow.org>
2024-03-08 15:47:06 -0500

    Version 1

--- v3_files/engines/logistic_reg.R
+++
@@ -1,6 +0,0 @@
-# tuned
-# model_spec <- logistic_reg(penalty = .0002, mixture = 1) |>
-#   set_engine("glmnet")
-
-model_spec <- logistic_reg(mode = "classification") |>
-  set_engine("glm", family = stats::binomial(link = "logit"))
--- v5_files/datasets/generate_datasets.R
+++
@@ -1,6 +0,0 @@
-root <- "/mnt/ecocast/projectdata/calanusclimate/src/final_datasets"
-
-# C. FINMARCHICUS
-destination <- "cfin_dw_ae.csv.gz"
-
-# where is the code to create the merged dataset? lol
